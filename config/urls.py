3f75c68563f95108dc955e416d837406f852261d
baobaba <151740387+amelliinn@users.noreply.github.com>
2026-02-12 16:24:15 +0700

    Неделя 2: настройка медиа, супер-админка и страница загрузки

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from gallery.views import home, about  # Импортируем обе функции
+from gallery.views import home, about, upload  # Импортируем обе функции
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('about/', about, name='about'),  # Добавляем этот маршрут
+    path('about/', about, name='about'),
+    path('upload/', upload, name='upload'),  # Добавляем этот маршрут
 ]
+# ВНИМАНИЕ: Эта магия работает только если DEBUG = True (режим разработки)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Asset  # импортируем наш класс
 
-# Регистрируем
-admin.site.register(Asset)
+# Используем декоратор @admin.register
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+# Какие поля показывать в таблице (колонки)
+    list_display = ('title', 'created_at', 'id')
+    # Добавляем строку поиска (поиск по названию)
+    search_fields = ('title',)
+    # Добавляем фильтр справа (по дате)
+    list_filter = ('created_at',)
+    # Поля, на которые можно кликнуть для входа в редактирование
+    list_display_links = ('title',)
